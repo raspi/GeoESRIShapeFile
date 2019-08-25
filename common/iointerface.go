31429e3a5535ab8c7ada620531949c035b6b39eb
raspi <pekka.jarvinen@gmail.com>
2019-08-25 19:31:39 +0300

    added memory based common temporary file ReadSeekCloser

--- common/iointerface.go
+++ common/iointerface.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"github.com/spf13/afero"
 	"io"
 	"time"
@@ -10,6 +11,34 @@ type ReadSeekCloser interface {
 	io.Reader
 	io.Seeker
 	io.Closer
+}
+
+func NewReadSeekCloser(b []byte) (rsc ReadSeekCloser, err error) {
+	base := afero.NewMemMapFs()
+	f, err := afero.TempFile(base, `/tmp`, `tmp_`)
+	if err != nil {
+		return f, err
+	}
+
+	wBytes, err := f.Write(b)
+	if err != nil {
+		return f, err
+	}
+
+	if wBytes != len(b) {
+		return f, fmt.Errorf(`couldn't write`)
+	}
+
+	offset, err := f.Seek(0, io.SeekStart)
+	if err != nil {
+		return f, err
+	}
+
+	if offset != 0 {
+		return f, fmt.Errorf(`couldn't seek to start`)
+	}
+
+	return f, nil
 }
 
 func OpenFile(fpath string) (ReadSeekCloser, error) {
